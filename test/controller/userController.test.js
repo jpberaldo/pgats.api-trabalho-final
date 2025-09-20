6f29048cda849d7d30a33470209fc2d7f81ac5c4
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2025-09-20 02:19:46 -0300

    Adicionando tratamento de erros nos Controllers e testes mockados

--- test/controller/userController.test.js
+++ test/controller/userController.test.js
@@ -4,6 +4,9 @@ const { expect } = require('chai');
 
 const app = require('../../app.js');
 
+//mock
+const userService = require('../../services/userService.js');
+
 
 describe('User Controller', () => {
 
@@ -33,6 +36,43 @@ describe('User Controller', () => {
             expect(resposta.status).to.equal(400);
             expect(resposta.body).to.have.property('error', 'Informe usuário e senha.');
 
+        });
+
+        it('Teste mockado: Quando registrar usuário com dados válidos o status de retorno será 201', async () => {
+            //Mockando função addUser da classe userController.js
+            const userServiceMock = sinon.stub(userService, 'addUser');
+            // userServiceMock.
+
+            const resposta = await request(app)
+                .post('/users/register')
+                .send({
+                    username: "usuario-teste",
+                    password: "senha-teste"
+                });
+
+            expect(resposta.status).to.equal(201);
+            expect(resposta.body).to.have.property('id', 1);
+
+            //reset mock
+            sinon.restore();
+
+        });
+
+        it.only('Teste mockado: Quando registrar usuário com username inválido', async () => {
+            const userServiceMock = sinon.stub(userService, 'addUser');
+            userServiceMock.throws(new Error('Informe usuário e senha.'));
+
+            const resposta = await request(app)
+                .post('/users/register')
+                .send({
+                    username: "",
+                    password: "senha-teste"
+                });
+
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Informe usuário e senha.');
+            
+            sinon.restore();
         });
 
     });
