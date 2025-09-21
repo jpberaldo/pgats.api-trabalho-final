c79736762eacbba9472188da2bbf809918d50227
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2025-09-21 12:47:25 -0300

    Adicionando beforeEach na productExternal

--- test/external/productExternal.test.js
+++ test/external/productExternal.test.js
@@ -4,19 +4,26 @@ const { expect } = require('chai');
 
 describe('Products External Tests', () => {
 
+    let token;
+
+    beforeEach(async () => {
+
+        const respostaLogin = await request('http://localhost:3000')
+            .post('/users/login')
+            .send({
+                username: "usuario-teste-product",
+                password: "senha-teste-product"
+            });
+
+        const body = typeof respostaLogin.body === 'string' ? JSON.parse(respostaLogin.body) : respostaLogin.body;
+        token = body.token;
+
+
+    });
+
     describe('/POST products', () => {
 
         it('Quando adicionar um produto com dados válidos o status de retorno será 201', async () => {
-
-            const respostaLogin = await request('http://localhost:3000')
-                .post('/users/login')
-                .send({
-                    username: "usuario-teste-product",
-                    password: "senha-teste-product"
-                });
-
-            const body = typeof respostaLogin.body === 'string' ? JSON.parse(respostaLogin.body) : respostaLogin.body;
-            const token = body.token;
 
             const resposta = await request('http://localhost:3000')
                 .post('/products')
